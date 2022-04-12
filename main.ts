9b56ebea0fc0ec74172fa9e982ec9bb69713fbdf
TrafalgarMat <103508401+TrafalgarMat@users.noreply.github.com>
2022-04-12 14:36:57 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
+function Smileys () {
+    if (n == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        if (n == 2) {
+            basic.showIcon(IconNames.Sad)
+        } else {
+            basic.showIcon(IconNames.Asleep)
+        }
+    }
+}
 let n = 0
+n = 0
 while (!(input.buttonIsPressed(Button.B))) {
     if (input.buttonIsPressed(Button.A)) {
         n += 1
         if (n > 3) {
             n = 1
         }
-        if (n == 1) {
-            basic.showIcon(IconNames.Happy)
-        } else {
-            if (n == 2) {
-                basic.showIcon(IconNames.Sad)
-            } else {
-                basic.showIcon(IconNames.Asleep)
-            }
-        }
+        Smileys()
     }
 }
 basic.showString("Votre humeur")
